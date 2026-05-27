9289717d075a05b9f256b731e698fc2fc7acf6b2
Felix <buhlerfelix72@gmail.com>
2026-05-27 12:12:29 +0200

    data: add weather DTO models

---
+++ weatherAPIOAuth/weatherAPI/Models/Dto/Weather.cs
@@ -0,0 +1,18 @@
+namespace weatherAPI.Models.Dto;
+public class Weather
+{
+    public required double Lat { get; set; }
+    public required double Lon { get; set; }   
+    public required string City { get; set; }
+    public string? Country { get; set; }
+    public double TemperatureC { get; set; }
+    public int Humidity { get; set; }
+    public double VisibilityKm { get; set; }
+    public string? Description { get; set; }
+    public string? Icon { get; set; }
+    public int WeatherId { get; set; }
+    public double WindSpeed { get; set; }
+    public string? Sunrise { get; set; }
+    public string? Sunset { get; set; }
+    public int TimezoneOffsetHours { get; set; }
+}
